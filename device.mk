572dc0efc36fff018b02dfd0be86c4ae14a5b717
Jack Matthews <jack@jackmatthe.ws>
2026-01-05 21:50:44 -0500

    mid1108: Inherit generic_ramdisk product

--- device.mk
+++ device.mk
@@ -8,6 +8,9 @@
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH)
 
+# Enforce generic ramdisk allow list
+$(call inherit-product, $(SRC_TARGET_DIR)/product/generic_ramdisk.mk)
+
 # Dynamic partitions
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
 
