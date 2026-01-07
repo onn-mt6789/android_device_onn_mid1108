07fc8910ae21258930372d862758b354a1177fa0
Jack Matthews <jack@jackmatthe.ws>
2026-01-07 02:04:59 -0500

    mid1108: Configure graphics stack * Contains PQ, composer/allocator/mapper

--- device.mk
+++ device.mk
@@ -87,6 +87,12 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     fastbootd
 
+# Display
+PRODUCT_PACKAGES += \
+    android.hardware.graphics.composer@2.3-service \
+    android.software.vulkan.deqp.level-2021-03-01.prebuilt.xml \
+    android.software.opengles.deqp.level-2021-03-01.prebuilt.xml
+
 # Permissions (features)
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.audio.low_latency.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.audio.low_latency.xml \
