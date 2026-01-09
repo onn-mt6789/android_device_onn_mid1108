cdb6bbf9b0e05b48f7ab2d8f48067a979b14a5b5
Jack Matthews <jack@jackmatthe.ws>
2026-01-09 08:27:21 -0500

    mid1108: link vendor.mediatek.hardware.pq@2.15-impl.so to libutils-v32.so and android.hardware.sensors@1.0-convert-shared.so

--- extract-files.py
+++ extract-files.py
@@ -80,6 +80,9 @@ blob_fixups: blob_fixups_user_type = {
     .add_needed('libbase_shim.so'),
     "vendor/lib64/hw/hwcomposer.mtk_common.so": blob_fixup()
     .add_needed('libprocessgroup_shim.so'),
+    'vendor/lib64/hw/mt6789/vendor.mediatek.hardware.pq@2.15-impl.so': blob_fixup()
+        .replace_needed('libsensorndkbridge.so', 'android.hardware.sensors@1.0-convert-shared.so')
+        .replace_needed('libutils.so', 'libutils-v32.so'),
 }  # fmt: skip
 
 module = ExtractUtilsModule(
